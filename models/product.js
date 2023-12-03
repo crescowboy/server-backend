5aa372175ec4a98d85d97bd67d028cdbc0116110
wcrespo <wellingtoncrespo2001@gmail.com>
2023-12-03 16:14:00 -0500

    creando modelo productModel

--- models/product.js
+++ models/product.js
@@ -1,12 +1,52 @@
+import { randomUUID } from 'node:crypto';
 import {readJSON} from '../utils'
-const product = readJSON('../product.json')
+const products = readJSON('../product.json')
 
 export class ProductModel{
-    static getAll({type}){
+    static getAll ({type}) {
         if(type){
             return products.filter(
                 product => product.type.some(g => g.toLowerCase() === type.toLowerCase())
             )
         }
+        return products
     }
+
+    
+static async getById ({id}) {
+    const product = products.find(product => product.id == id)
+    return product
+}
+
+static async create({input}){
+    const newProduct = {
+        id: randomUUID(),
+        ...input
+    }
+
+    products.push(newProduct)
+
+    return newProduct
+}
+
+static async delete ({id}){
+    const productIndex = products.findIndex(product => product.id == id);
+    if(productIndex == -1) return false
+
+        products.splice(productIndex, 1)
+        return true
+    
+}
+
+static async update ({id, input}){
+    const productIndex = products.findIndex(product => product.id == id)
+    if (productIndex == -1) return false;
+
+    products[productIndex] = {
+        ...products[productIndex],
+        ...input
+    }
+
+    return products[productIndex]
+}
 }
--- routes/products.js
+++ routes/products.js
@@ -1,25 +1,22 @@
 import {Router} from 'express'
-import { readJSON } from './utils.js';
-import { randomUUID } from 'node:crypto';
 import { ProductModel } from '../models/product.js';
 const router = Router()
-const products = readJSON('./json/products.json')
 
-router.get('/', (req, res)=>{
+router.get('/', async (req, res)=>{
     const { type } = req.query
-    const products = ProductModel.getAll({type})
+    const products = await ProductModel.getAll({type})
     res.json(products)
 })
 
-router.get('/:id', (req,res)=>{
+router.get('/:id', async (req,res)=>{
     const {id} = req.params
-    const product = products.find(product => product.id == id)
+    const product = await ProductModel.getById({id})
     if(product) return res.json(product)
 
     res.status(404).json({message: "No se encontro el producto"})
 })
 
-router.post('/', (req,res)=>{
+router.post('/', async (req,res)=>{
     const result = validateProduct(req.body) 
     console.log(req.body)
 
@@ -29,58 +26,41 @@ router.post('/', (req,res)=>{
 
     
 
-    const newProduct = {
-        id: randomUUID(),
-        ...result.data
-    }
-
-    // console.log(newProduct)
-
-    products.push(newProduct)
+   const newProduct = await ProductModel.create({input: result.data})
     res.status(201).json(newProduct)
 })
 
-router.delete('/:id', (req,res)=>{
+router.delete('/:id', async (req,res)=>{
     
     
     const {id} = req.params;
-    const productIndex = products.findIndex(product => product.id == id);
+    const result = await ProductModel.delete({id})
 
-    if(productIndex == -1){
+    if(result == false){
         return res.status(404).json({message: 'product not found'})
     }
 
-    products.splice(productIndex, 1)
+    
 
     return res.json({message: 'Product deleted'})
 
 })
 
-router.patch('/:id', (req,res)=>{
+router.patch('/:id', async (req,res)=>{
     const result = validatePartialProduct(req.body);
     // console.log(req.body)
 
     if(!result.success){
-        console.log(result);
         return res.status(400).json({error: JSON.parse(result.error.message)})
     }
 
     const {id} = req.params
     // console.log(id)
-    const productIndex = products.findIndex(product => product.id == id)
+    const updatedProduct = await ProductModel.update({id, input: result.data})
     // console.log(productIndex)
-    if(productIndex === -1){
-        return res.status(404).json({message: 'producto no encontrado'})
-    } 
+     
 
-    const updateProduct = {
-        ...products[productIndex],
-        ...result.data
-    }
-
-    products[productIndex] = updateProduct
-
-    res.status(201).json(updateProduct)
+    return res.json(updatedProduct)
 })
 
 export default router
